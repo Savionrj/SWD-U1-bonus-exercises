8445188f5ad227694f53aaee0d730e56abffc73a
Savionrj <savionrj@gmail.com>
2025-04-29 22:23:35 -0500

    jest examples

--- unit-testing/examples.js
+++ unit-testing/examples.js
@@ -1,14 +1,14 @@
 /** UNIT TESTING **/
 
 /* 
-	EXAMPLE: In the corresponding spec file, write a test for the MS_PER_YEAR constant below.
+  EXAMPLE: In the corresponding spec file, write a test for the MS_PER_YEAR constant below.
 */
 
 let MS_PER_YEAR = 1000 * 60 * 60 * 24 * 365.25;
 
 
 /* 
-	EXAMPLE: In the corresponding test file, write a test for each property in the object below. Fix any mistakes that are uncovered by the tests.
+  EXAMPLE: In the corresponding test file, write a test for each property in the object below. Fix any mistakes that are uncovered by the tests.
 */
 
 let bucky = {
@@ -36,24 +36,28 @@ let bucky = {
 /** TEST-DRIVEN DEVELOPMENT **/
 
 /*
-	EXAMPLE: Practice TDD by writing each test before adding code to the function below.
+  EXAMPLE: Practice TDD by writing each test before adding code to the function below.
     The function should return true or false depending on whether all of its characters are alphabetical or not.
 */
 
 function isAlphaOnly(str) {
-    // TODO: Fix for additional edge case
-    if (str.trim() === '') {
-        return false;
+  // TODO: Fix for additional edge case
+  if (typeof str !== 'string') {
+    return false;
+  }
+  if (str.trim() === '') {
+    return false;
+  }
+  let allowed = 'abcdefghijklmnopqrstuvwxyz ';
+  for (let char of str) {
+    if (!allowed.includes(char.toLowerCase())) {
+      return false;
     }
-    let allowed = 'abcdefghijklmnopqrstuvwxyz ';
-    for (char of str) {
-        if (!allowed.includes(char.toLowerCase())) {
-        return false;
-        }
-    }
-    return true;
+  }
+  return true;
 }
 
+console.log(isAlphaOnly('steve rodgers'));
 
 // EXAMPLE: Export items to be tested
 module.exports = {
